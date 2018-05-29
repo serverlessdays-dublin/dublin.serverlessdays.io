09237b19077e4e99c985ca235f0067452217d3c2
Luciano Mammino <lucianomammino@gmail.com>
2018-05-29 22:47:07 +0100

    bootstrapped with gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'ServerlessDays Dublin',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>This is a fantasti-sh work in progress web page for ServerlessDays Dublin Edition</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
